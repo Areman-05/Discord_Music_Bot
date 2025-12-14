73480b271e2673b969b53c0b1ce92dbd1e249e38
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-14 20:49:16 +0100

    Add NowplayingHandler for updating now playing messages

---
+++ src/main/java/com/soundbot/audio/NowplayingHandler.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2018 SoundBot Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.soundbot.audio;
+
+import com.soundbot.Bot;
+import com.soundbot.entities.Pair;
+import com.soundbot.settings.Settings;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.exceptions.PermissionException;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
+
+/**
+ *
+ * @author SoundBot Contributors
+ */
+public class NowplayingHandler
+{
+    private final Bot bot;
+    private final HashMap<Long,Pair<Long,Long>> lastNP; // guild -> channel,message
+    
+    public NowplayingHandler(Bot bot)
+    {
+        this.bot = bot;
+        this.lastNP = new HashMap<>();
+    }
+    
+    public void init()
+    {
+        if(!bot.getConfig().useNPImages())
+            bot.getThreadpool().scheduleWithFixedDelay(() -> updateAll(), 0, 5, TimeUnit.SECONDS);
+    }
+    
+    public void setLastNPMessage(Message m)
+    {
+        lastNP.put(m.getGuild().getIdLong(), new Pair<>(m.getTextChannel().getIdLong(), m.getIdLong()));
+    }
+    
+    public void clearLastNPMessage(Guild guild)
+    {
+        lastNP.remove(guild.getIdLong());
+    }
+    
+    private void updateAll()
+    {
+        Set<Long> toRemove = new HashSet<>();
+        for(long guildId: lastNP.keySet())
+        {
+            Guild guild = bot.getJDA().getGuildById(guildId);
+            if(guild==null)
+            {
+                toRemove.add(guildId);
+                continue;
+            }
+            Pair<Long,Long> pair = lastNP.get(guildId);
+            TextChannel tc = guild.getTextChannelById(pair.getKey());
+            if(tc==null)
+            {
+                toRemove.add(guildId);
+                continue;
+            }
+            AudioHandler handler = (AudioHandler)guild.getAudioManager().getSendingHandler();
+            Message msg = handler.getNowPlaying(bot.getJDA());
+            if(msg==null)
+            {
+                msg = handler.getNoMusicPlaying(bot.getJDA());
+                toRemove.add(guildId);
+            }
+            try 
+            {
+                tc.editMessageById(pair.getValue(), msg).queue(m->{}, t -> lastNP.remove(guildId));
+            } 
+            catch(Exception e) 
+            {
+                toRemove.add(guildId);
+            }
+        }
+        toRemove.forEach(id -> lastNP.remove(id));
+    }
+    
+    public void onMessageDelete(Guild guild, long messageId)
+    {
+        Pair<Long,Long> pair = lastNP.get(guild.getIdLong());
+        if(pair==null)
+            return;
+        if(pair.getValue() == messageId)
+            lastNP.remove(guild.getIdLong());
+    }
+    
+    public void updateTopic(long guildId, AudioTrack track, boolean paused)
+    {
+        Guild guild = bot.getJDA().getGuildById(guildId);
+        if(guild==null)
+            return;
+        Settings settings = bot.getSettingsManager().getSettings(guild);
+        TextChannel tc = settings.getTextChannel(guild);
+        if(tc==null || !guild.getSelfMember().hasPermission(tc, Permission.MANAGE_CHANNEL))
+            return;
+        String topic = tc.getTopic();
+        if(topic==null || topic.isEmpty())
+            topic = "\u200B";
+        String playing = (paused ? "\u23F8" : "\u25B6") + " " + FormatUtil.progressBar((double)track.getPosition()/track.getDuration()) + " `[" 
+                + FormatUtil.formatTime(track.getPosition()) + "/" + FormatUtil.formatTime(track.getDuration()) + "]` " 
+                + FormatUtil.filter(track.getInfo().title);
+        if(topic.equals(playing))
+            return;
+        int index = playing.length() - 250;
+        String shorter = index >= playing.length() - 20 ? playing : playing.substring(0, index) + "... " + FormatUtil.filter(track.getInfo().title);
+        try 
+        {
+            tc.getManager().setTopic(shorter).queue();
+        } 
+        catch(PermissionException | RateLimitedException ignored) {}
+    }
+}
+
