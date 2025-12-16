8248751e05775062a0b169ecb71ede41178178e8
Pablo Arenas <pablo.arenas@gracia.lasalle.cat>
2025-12-16 22:41:33 +0100

    Agregar validaciÃ³n null al encolar tracks

--- src/main/java/com/soundbot/TrackScheduler.java
+++ src/main/java/com/soundbot/TrackScheduler.java
@@ -20,6 +20,7 @@ public class TrackScheduler extends AudioEventAdapter {
     }
     
     public void queue(AudioTrack track) {
+        if (track == null) return;
         if (!player.startTrack(track, true)) {
             queue.offer(track);
         }
